c5476dd19a883b7240d9ff09c4fd431a01c269f8
Tosh <git@chaoticenginerd.com>
2022-12-28 07:26:37 +0545

    Update telescope keybinding to prevent clash with lsp keybindings

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -75,10 +75,10 @@ vim.keymap.set("n", "<leader>l", "<cmd>wincmd l<cr>")
 
    -- Telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>tf", builtin.find_files, {})
+vim.keymap.set("n", "<leader>tb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>ts", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>th", builtin.help_tags, {})
 
 vim.opt.background = "light"
 vim.cmd("colorscheme oxocarbon")
